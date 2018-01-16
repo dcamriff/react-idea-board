28a958ad0acae3e90022755c6017c87a6627abfe
dcamriff <driffelatl@gmail.com>
2018-01-16 12:24:27 -0500

    set up Heroku for express and react

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const app = express();
+require("dotenv").config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.static(__dirname + '/client/build/'))
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true
@@ -10,20 +14,20 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const connection = mongoose.connection;
 connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully');    
-}); 
+  console.log('Mongoose Connected Successfully') 
+})
 
 // If the connection throws an error
 connection.on('error', (err) => {
-  console.log('Mongoose default connection error: ' + err);
-}); 
-
-app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
+  console.log('Mongoose default connection error: ' + err)
 })
 
-const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json())
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log("Magic happening on port " + PORT)
 })
